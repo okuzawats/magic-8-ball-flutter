afb4c0e98e496cdfaba364f5561c3c6f9d538f4e
okuzawats <okuzawats@gmail.com>
2020-01-16 21:43:55 +0900

    画像にクリックイベントを追加

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,13 @@ class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Image.asset('images/ball1.png')),
+      child: Center(
+          child: FlatButton(
+        onPressed: () {
+          print('I got clicked');
+        },
+        child: Image.asset('images/ball1.png'),
+      )),
     );
   }
 }
